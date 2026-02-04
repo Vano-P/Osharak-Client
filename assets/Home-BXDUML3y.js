fed93fcbc6ed38df4d4a00102716dc088a113853
Vano <Van.Poghosyan@gmail.com>
2026-02-04 19:38:10 +0400

    Updates

--- assets/Home-BXDUML3y.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Crrd9SCa.js";const e=()=>s.jsx("section",{className:"section"});export{e as default};
---
+++ assets/Home-CeH-dbtS.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-hJVrv7Ce.js";const e=()=>s.jsx("section",{className:"section"});export{e as default};
